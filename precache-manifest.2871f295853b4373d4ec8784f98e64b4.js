a0fb59f642c8ee403ddb7a9d78d90d5530c16a16
sodiqlawal <sodiqlawal95@gmail.com>
2020-01-15 15:07:27 +0100

    Updates

---
+++ precache-manifest.2871f295853b4373d4ec8784f98e64b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2df14c5882bcb7fc81d2f524e0162d",
+    "url": "/exolve/index.html"
+  },
+  {
+    "revision": "a8f12ea9256140a89d23",
+    "url": "/exolve/static/css/main.e7eef05f.chunk.css"
+  },
+  {
+    "revision": "af97d184bb387ab9929e",
+    "url": "/exolve/static/js/2.6479d9b6.chunk.js"
+  },
+  {
+    "revision": "76d0a2b8004c5144545d6c44f167ed95",
+    "url": "/exolve/static/js/2.6479d9b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8f12ea9256140a89d23",
+    "url": "/exolve/static/js/main.554e9d44.chunk.js"
+  },
+  {
+    "revision": "60897791887f5e8e6f5c",
+    "url": "/exolve/static/js/runtime-main.adce0cc5.js"
+  },
+  {
+    "revision": "d70378a5304fd0a29f8321fb08931012",
+    "url": "/exolve/static/media/cartoon.d70378a5.png"
+  }
+]);
